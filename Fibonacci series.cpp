a148fbd727b78091deef99a2b26500fcafa9d973
kopiladevkota <168613230+kopiladevkota@users.noreply.github.com>
2024-09-02 08:47:58 +0545

    Add files via upload

---
+++ Fibonacci series.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+// Function to print Fibonacci series
+void printFibonacci(int n) {
+    int t1 = 0, t2 = 1, nextTerm;
+    
+    printf("Fibonacci Series: ");
+    
+    for (int i = 1; i <= n; ++i) {
+        printf("%d ", t1);
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+    }
+    
+    printf("\n");
+}
+
+int main() {
+    int n;
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+    
+    printFibonacci(n);
+    
+    return 0;
+}
