82f04ce0f0b9b7d6182007e8b81b086e05e7d77a
kopiladevkota <168613230+kopiladevkota@users.noreply.github.com>
2024-09-04 07:23:18 +0545

    Add files via upload

---
+++ Insertion sort.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+// Function to implement Insertion Sort
+void insertionSort(int arr[], int n) {
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+
+        // Move elements of arr[0..i-1], that are greater than key,
+        // to one position ahead of their current position
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+// Function to print an array
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {102, 111, 130, 15, 206};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    printf("Unsorted array: \n");
+    printArray(arr, n);
+
+    insertionSort(arr, n);
+
+    printf("Sorted array: \n");
+    printArray(arr, n);
+    
+    return 0;
+}
